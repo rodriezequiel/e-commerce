c31af6765bcd16a7371e8cd5ef8ee2baffeceabe
julianianni <julianianni@gmail.com>
2021-06-28 17:52:03 -0300

    chauporhola

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,9 @@
-
 function App() {
   return (
-    <div className="App">
-      <h1>hola</h1>
+    <div className='App'>
+      <h1>Chau</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
