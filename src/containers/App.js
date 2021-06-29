566148cb4e5d376b7644900675c227a3ec6340e7
julianianni <julianianni@gmail.com>
2021-06-29 12:03:17 -0300

    agregado newls

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,9 @@
 function App() {
   return (
-    <div className="App">
-      <h1>Hola mundo!</h1>
+    <div className='App'>
+      <h1>Hola mundo dale newwwwwwwwwwwwls!</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
