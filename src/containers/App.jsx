d9fd0cae745ce1afbc4ee50f80455be2bc4de91e
jackelin perez <jmperezchahua@gmail.com>
2021-07-01 17:20:39 -0500

    maquetado Signin and Signup

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,18 +1,22 @@
 import React from "react";
-import { Route, Switch, Redirect} from "react-router";
-import Home from "./Home"
+import { Route, Switch, Redirect } from "react-router";
+import Home from "./Home";
 import Shop from "./Shop";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
 
 function App() {
   return (
     <div>
-        <Switch>
-          <Route exact path="/home" component={Home} />
-          <Route exct path="/shop" component={Shop}/>
-          <Redirect from="/" to="/home" />
-        </Switch>
+      <Switch>
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/shop" component={Shop} />
+        <Route exact path="/signin" component={SignIn} />
+        <Route exact path="/signup" component={SignUp} />
+        <Redirect from="/" to="/home" />
+      </Switch>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
