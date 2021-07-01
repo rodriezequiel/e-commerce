945f51e382bcdea4a0fdfd573c8719c7731935b9
ivanmoroni92 <ivan.mor92@gmail.com>
2021-07-01 12:10:48 -0300

    category

--- api/routes/ProductRoutes.js
+++ api/routes/ProductRoutes.js
@@ -1,12 +1,22 @@
-const express = require('express')
-const router = express.Router()
-const Cart = require('../db/CartModels')
-const Product = require('../db/ProductModels')
-const User = require('../db/UserModels')
-const CartItems = require('../db/CartItemsModels')
+const express = require("express");
+const router = express.Router();
+const Cart = require("../db/CartModels");
+const Product = require("../db/ProductModels");
+const User = require("../db/UserModels");
+const CartItems = require("../db/CartItemsModels");
+const Category = require("../db/CategoryModels");
 
-router.get('/', (req, res) => {
-  Product.findAll().then((products) => res.send(products))
-})
+router.get("/all", (req, res) => {
+  Product.findAll().then((products) => res.send(products));
+});
 
-module.exports = router
+router.get("/single/:id", (req, res) => {
+  let id = req.params.id;
+  Product.findByPk(id).then((product) => res.send(product));
+});
+
+router.get("/allcategory", (req, res) => {
+  Category.findAll().then((cartegorys) => res.send(cartegorys));
+});
+
+module.exports = router;
--- api/seed.js
+++ api/seed.js
@@ -1,56 +1,68 @@
-const { User, Product, Order, Cart } = require('./db/index')
+const { User, Product, Order, Cart, Category } = require("./db/index");
+
+const categorys = [
+  { name: "surf" },
+  { name: "skate" },
+  { name: "hombre" },
+  { name: "mujer" },
+];
 
 const user = [
   {
-    firstName: 'Julian',
-    lastName: 'Perez',
-    email: 'julian@test.com',
-    password: '1234',
+    firstName: "Julian",
+    lastName: "Perez",
+    email: "julian@test.com",
+    password: "1234",
     isAdmin: false,
   },
   {
-    firstName: 'Ivan',
-    lastName: 'Moroni',
-    email: 'ivan@test.com',
-    password: '12345',
+    firstName: "Ivan",
+    lastName: "Moroni",
+    email: "ivan@test.com",
+    password: "12345",
     isAdmin: false,
   },
   {
-    firstName: 'Rodri',
-    lastName: 'Bacigalupo',
-    email: 'rodri@test.com',
-    password: 'rodri',
+    firstName: "Rodri",
+    lastName: "Bacigalupo",
+    email: "rodri@test.com",
+    password: "rodri",
     isAdmin: false,
   },
-]
+];
 
 const product = [
   {
-    name: 'campera de prueba',
+    name: "campera de prueba",
     price: 100,
     stock: 50,
   },
   {
-    name: 'mochila',
+    name: "mochila",
     price: 200,
     stock: 10,
   },
   {
-    name: 'tabla de surf',
+    name: "tabla de surf",
     price: 150,
     stock: 2,
   },
-]
+];
 
-const cart = [{ state: 'inProgress' }, { state: 'inProgress' }]
+const cart = [{ state: "inProgress" }, { state: "inProgress" }];
 User.bulkCreate(user, { returning: true }).then(
-  (user) => console.log('usuarios creados')
+  (user) => console.log("usuarios creados")
   // process.exit()
-)
+);
 Product.bulkCreate(product, { returning: true }).then(
-  (product) => console.log('product creados')
+  (product) => console.log("product creados")
   // process.exit()
-)
+);
+
+Category.bulkCreate(categorys, { returning: true }).then(
+  (product) => console.log("categorys creados")
+  // process.exit()
+);
 
 // Cart.bulkCreate(cart, { returning: true }).then((cart) =>
 //   console.log('carritos creados ok')
