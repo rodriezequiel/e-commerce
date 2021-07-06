30146dce7ad6eb38830bab921b589c30f5a67e88
jackelin perez <jmperezchahua@gmail.com>
2021-07-06 13:40:35 -0500

    style singleProduct

---
+++ src/utils/globals.js
@@ -0,0 +1,10 @@
+export const colors = ["blue", "red", "green"];
+export const colorClass = {
+  empty: "bi bi-circle-fill fs-3 mx-2",
+  fill: "bi bi-check-circle-fill fs-3 mx-2",
+};
+export const talles = ["S", "M", "L"];
+export const talleClass = {
+  empty: "bi bi-circle fs-4 mx-2",
+  fill: "bi bi-check-circle fs-4 mx-2",
+};
