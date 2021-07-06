c79215320b9e601a3ecb89695c6a088164231239
ramirocaruso <ramicaruso5@gmail.com>
2021-07-06 19:03:11 -0300

    branch

--- src/containers/SignIn.jsx
+++ src/containers/SignIn.jsx
@@ -1,81 +1,105 @@
-import { Link, useHistory } from 'react-router-dom'
-import { useDispatch } from 'react-redux';
-import axios from 'axios'
-import { useState } from 'react';
-import Navbar from '../components/Navbar'
-import { getUser } from '../state/user';
+import { Link, useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import axios from "axios";
+import { useState } from "react";
+
+import Navbar from "../components/Navbar";
+import { getUser } from "../state/user";
 
 export default function SignIn() {
-
   const [input, setInput] = useState({
     password: "",
     email: "",
-  }); 
+  });
+ 
   const history = useHistory();
-  const dispatch= useDispatch();
+  const dispatch = useDispatch();
   console.log(input);
 
-  const changeHandler = event => {
+  const changeHandler = (event) => {
+ 
+   
     const { target } = event;
     setInput({ ...input, [target.name]: target.value });
   };
 
-  const submitHandler = event => {
-    event.preventDefault();
-    return axios.post("/api/auth/login", input)
-    .then(res => res.data)
-    .then(user=> dispatch(getUser(user)))
-    .then(res => history.push("/home"))
-    .catch(err => alert('datos incorrectos')) 
+  const submitHandler = (event) => {
+    event.preventDefault(); 
+   return axios
+        .post("/api/auth/login", input)
+        .then((res) => res.data)
+        .then((user) => dispatch(getUser(user)))
+        .then((res) => history.push("/home"))
+        .catch((err) => alert("tus datos son incorrectos"));
   };
+
 
   return (
     <div>
       <Navbar transparent={false} />
-      <div className='container-fluid py-4 d-grid gap-3 d-flex justify-content-center'>
-        <div className='col-4 bg-light border align-center p-3'>
-          <h1 className='text-center'>Sign In</h1>
-          <form className='form-style justify-content-between' onChange={changeHandler} onSubmit={submitHandler}>
-            <div className='mb-3'>
-              <label htmlFor='email' className='form-label'>
+      <div className="container-fluid py-4 d-grid gap-3 d-flex justify-content-center">
+        <div className="col-4 bg-light border align-center p-3">
+          <h1 className="text-center">Sign In</h1>
+          <form
+            id="form"
+            className="form-style justify-content-between"
+            onSubmit={submitHandler}
+          >
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
                 Email*
               </label>
-              <input type='text' className='form-control' id='email' name='email'/>
+              <input
+                type="text"
+                className="form-control"
+                id="email"
+                name="email"
+                onChange={changeHandler}
+              />
+            
             </div>
-            <div className='mb-3'>
-              <label htmlFor='password' className='form-label'>
+
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
                 Password *
               </label>
-              <input type='password' className='form-control' id='password' name='password'/>
-            </div>
-            <div className='mb-3 form-check'>
               <input
-                type='checkbox'
-                className='form-check-input'
-                id='exampleCheck1'
+                type="password"
+                className="form-control"
+                id="password"
+                name="password"
+                onChange={changeHandler}
               />
-              <label className='form-check-label' htmlFor='exampleCheck1'>
+            
+            </div>
+            <div className="mb-3 form-check">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="exampleCheck1"
+              />
+              <label className="form-check-label" htmlFor="exampleCheck1">
                 Check me out
               </label>
             </div>
-            <button type='submit' className='btn btn-dark my-2'>
+            <button type="submit" className="btn btn-dark my-2">
               Submit
             </button>
-            <h3 className='text-center'>Or</h3>
-            <div className='mb-3'>
-              <button type='submit' className='btn btn-danger my-2'>
+            <h3 className="text-center">Or</h3>
+            <div className="mb-3">
+              <button type="submit" className="btn btn-danger my-2">
                 Google
               </button>
-              <button type='submit' className='btn btn-primary my-2'>
+              <button type="submit" className="btn btn-primary my-2">
                 Facebook
               </button>
             </div>
 
             <h3>Don't have an account?</h3>
             <Link
-              style={{ color: 'red' }}
-              className='nav-link active text-center'
-              to='/signup'
+              style={{ color: "red" }}
+              className="nav-link active text-center"
+              to="/signup"
             >
               Sign Up
             </Link>
@@ -83,5 +107,5 @@ export default function SignIn() {
         </div>
       </div>
     </div>
-  )
+  );
 }
